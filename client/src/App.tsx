d05e531b5d4acd5ece5483ff779ea0c60573e01f
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-07-12 12:13:02 -0600

    feat: add client code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+import "./App.css";
+
+import axios from "axios";
+import { saveAs } from "file-saver";
+
+type Data = {
+  name: string;
+  receiptId: 0;
+  price1: 0;
+  price2: 0;
+};
 
 function App() {
+  const [data, setData] = useState<Data>({} as Data);
+
+  const handleChange = ({
+    target: { value, name },
+  }: ChangeEvent<HTMLInputElement>) =>
+    setData((prevData) => ({ ...prevData, [name]: value }));
+
+  const createAndDownloadPdf = () => {
+    axios
+      .post("/create-pdf", data)
+      .then(() => axios.get("fetch-pdf", { responseType: "blob" }))
+      .then((res) => {
+        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+
+        saveAs(pdfBlob, "newPdf.pdf");
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Receipt ID"
+        name="receiptId"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Price 1"
+        name="price1"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Price 2"
+        name="price2"
+        onChange={handleChange}
+      />
+      <button onClick={createAndDownloadPdf}>Download PDF</button>
     </div>
   );
 }
